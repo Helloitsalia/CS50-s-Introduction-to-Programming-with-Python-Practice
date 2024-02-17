ad35737e5e85cb057e09a5afa64ee954658c5d21
Saliha Kansız Yazıcı <62435742+Helloitsalia@users.noreply.github.com>
2024-02-17 21:27:29 +0000

    Conditionals

---
+++ grade.py
@@ -0,0 +1,14 @@
+score = int(input("score:"))
+
+if 90 <= score ans score <= 100:
+    print("Grede:A")
+elif score >= 80:
+    print("Grade:B")
+elif score >= 70:
+    print("Grade:C")
+elif score >= 60:
+    print("Grade:D")
+else:
+    print("Grade:F")
+
+
