da6803fd27b11788b95a349fd1fbd93658119dd7
Saliha Kansız Yazıcı <62435742+Helloitsalia@users.noreply.github.com>
2024-02-18 19:46:27 +0000

    next

---
+++ mario.py
@@ -0,0 +1,22 @@
+# def main():
+#     print_row(4)
+
+# def print_row(width):
+#     print("?")
+
+# main()
+
+def main():
+    print_square(3)
+
+def print_square(size):
+    for i in range(size):
+        # for j in range(size):
+        #     print("#", end="")
+        print_row(size)
+
+
+def print_row(width):
+    print("#" * width)
+    
+main()
