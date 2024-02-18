f1af8ed589367467d1123445cb06a0015de504e3
Saliha Kansız Yazıcı <62435742+Helloitsalia@users.noreply.github.com>
2024-02-18 21:07:27 +0000

    pass

--- number.py
+++ number.py
@@ -5,12 +5,26 @@
 # else:
 #     print(f"x is {x}")
 
-while True:
-    try:
-        x= int(input("What's x?"))
-    except ValueError:
-        print("x is not a integer")
-    else:
-        break
+# while True:
+#     try:
+#         x = int(input("What's x?"))
+#     except ValueError:
+#         print("x is not a integer")
+#     else:
+#         break
 
-print(f"x is {x}")
+# print(f"x is {x}")
+
+
+def main():
+    x = get_int()
+    print (f"x is {x}")
+
+def get_int():
+    while True:
+        try:
+            return int(input("What's x?"))
+        except ValueError:
+            pass
+
+main()
