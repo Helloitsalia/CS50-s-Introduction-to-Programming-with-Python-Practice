e0b96f9dec5ffae4958d5ba599a1d70d29b7ea8b
Saliha Kansız Yazıcı <62435742+Helloitsalia@users.noreply.github.com>
2024-02-18 20:57:40 +0000

    loop

--- number.py
+++ number.py
@@ -1,5 +1,16 @@
-try: 
-    x = int(input("What's x?"))
-    print(f"x is {x}")
-except ValueError:
-    print("x is not a integer")
+# try: 
+#     x = int(input("What's x?"))
+# except ValueError:
+#     print("x is not a integer")
+# else:
+#     print(f"x is {x}")
+
+while True:
+    try:
+        x= int(input("What's x?"))
+    except ValueError:
+        print("x is not a integer")
+    else:
+        break
+
+print(f"x is {x}")
