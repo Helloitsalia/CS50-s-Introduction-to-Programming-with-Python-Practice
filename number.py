2346d46a18ee53061e5d1c686ca45da6f4d7a7ff
Saliha Kansız Yazıcı <62435742+Helloitsalia@users.noreply.github.com>
2024-02-18 21:13:23 +0000

    next

--- number.py
+++ number.py
@@ -16,14 +16,27 @@
 # print(f"x is {x}")
 
 
+# def main():
+#     x = get_int()
+#     print (f"x is {x}")
+
+# def get_int():
+#     while True:
+#         try:
+#             return int(input("What's x?"))
+#         except ValueError:
+#             pass
+
+# main()
+
 def main():
-    x = get_int()
+    x = get_int("What's x?")
     print (f"x is {x}")
 
-def get_int():
+def get_int(prompt):
     while True:
         try:
-            return int(input("What's x?"))
+            return int(input(prompt))
         except ValueError:
             pass
 
