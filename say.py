b9521159bd01d1e72d8a71e3438d0ad68294b120
Saliha Kansız Yazıcı <62435742+Helloitsalia@users.noreply.github.com>
2024-03-04 23:08:40 +0000

    next

--- say.py
+++ say.py
@@ -1,7 +1,15 @@
-import cowsay
+# import cowsay
+# import sys
+
+# if len(sys.argv)==2:
+#     cowsay.cow("hello,"+ sys.argv[1])
+#     # cowsay.trex("hello,"+ sys.argv[1])
+    
+
+
 import sys
 
-if len(sys.argv)==2:
-    cowsay.cow("hello,"+ sys.argv[1])
-    # cowsay.trex("hello,"+ sys.argv[1])
-    
+from sayings import hello
+
+if len (sys.argv) == 2:
+    hello(sys(argv[1]))
